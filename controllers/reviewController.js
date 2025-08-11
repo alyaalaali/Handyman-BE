9b570a2a6cb8432c3c669fc2704cbd1412863c96
abdkhalid24 <busaud973@gmail.com>
2025-08-11 16:32:15 +0300

    Some changes in the data of the reviewController.js

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,32 +1,34 @@
-const Review = require("../models/Review")
-const Request = require("../models/Request")
-
+const Review = require("../models/Review");
+const Request = require("../models/Request");
 
 const createReview = async (req, res) => {
   try {
-    const { Rating, description, requestId, userId } = req.body
+    console.log(req.body);
+    const { rating, description, requestId } = req.body;
+    const userId = res.locals.payload.id;
+    const numberRating = parseInt(rating);
     // needs to get the userID from token
     const review = await Review.create({
-      Rating,
+      Rating: numberRating,
       description,
       requestId,
-      userId
-    })
-    res.send(review)
+      userId,
+    });
+    res.send(review);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 const deleteReview = async (req, res) => {
   try {
-    const { id } = req.params
+    const { id } = req.params;
     // needs user validation check
-    const deletedReview = await Review.findByIdAndDelete(id)
+    const deletedReview = await Review.findByIdAndDelete(id);
 
-    res.send("Review deleted successfully")
+    res.send("Review deleted successfully");
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
-module.exports = { createReview, deleteReview }
+};
+module.exports = { createReview, deleteReview };
